7cf0e4062b4af8787edd731362b4a67740f807af
carl <12252402@ucdconnect.ie>
2013-11-14 15:13:05 +0000

    Layout for timer, and graphics assets, some changes to manifest themes

--- JuggerMatch/src/com/group5/juggermatch/MatchTimer.java
+++ JuggerMatch/src/com/group5/juggermatch/MatchTimer.java
@@ -1,31 +1,19 @@
 package com.group5.juggermatch;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
-import android.annotation.TargetApi;
-import android.os.Build;
+import android.view.Window;
 
 public class MatchTimer extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_match_timer);
-		// Show the Up button in the action bar.
-		setupActionBar();
-	}
-
-	/**
-	 * Set up the {@link android.app.ActionBar}, if the API is available.
-	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	private void setupActionBar() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			getActionBar().setDisplayHomeAsUpEnabled(true);
-		}
 	}
 
 	@Override
