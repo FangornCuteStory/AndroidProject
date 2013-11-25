fcf3c171a21799770760af5e7ea5b63ea1add192
oneshot2shots <daniel.o-keeffe.1@ucdconnect.ie>
2013-11-25 15:04:57 +0000

    Create timerTask.java

---
+++ JuggerMatch/src/com/group5/juggermatch/timerTask.java
@@ -0,0 +1,50 @@
+
+package com.group5.juggermatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+public class timerTask extends Activity {
+
+Timer timer;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.buzz); 
+           
+          
+        timer = new Timer();
+        timer.schedule(new RemindTask(),
+                       0,        //initial delay
+                       1*3000);  //subsequent rate
+            
+        }
+
+                 
+         public Beep(){}; / Not sure this actually does anything
+            
+             class RemindTask extends TimerTask {
+
+                                        public void run() {
+                                
+                            MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.beep);        
+                                mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                                mPlayer.start();
+                                                    
+                    }
+              }
+             
+
+protected void onPause(){
+
+    super.onPause();
+    timer.cancel();
+
+}
+
+
+  
