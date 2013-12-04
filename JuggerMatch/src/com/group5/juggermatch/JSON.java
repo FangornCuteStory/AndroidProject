5cc19d6255b7477ec3520b7df5be692088648be3
oneshot2shots <daniel.o-keeffe.1@ucdconnect.ie>
2013-12-04 14:30:43 +0000

    Create JSON.java

---
+++ JuggerMatch/src/com/group5/juggermatch/JSON.java
@@ -0,0 +1,25 @@
+package com.group5.juggermatch;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+
+import android.util.Log;
+
+public class JSON {
+
+	public long toPhpFile(Match match){
+	
+	
+	
+	}
+	
+	}
