1a55988bf076df2f9b3fb1159d5bcd31171204e3
Somebody-stumbled <a05bf5be@ucdconnect.ie>
2013-12-06 18:21:21 +0000

    splashscreen

--- JuggerMatch/src/com/group5/juggermatch/MainMenu.java
+++ JuggerMatch/src/com/group5/juggermatch/MainMenu.java
@@ -11,17 +11,45 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainMenu extends Activity {
-
+	protected Dialog mSplashDialog; // Needed for SplashScreen
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+    	  
+	    }
+        else{   
+        	this.requestWindowFeature(Window.FEATURE_NO_TITLE); // RC: this removes black bar at the top of activities 
+        }
+   
+        //Splash Code Start
+        MyStateSaver data = (MyStateSaver) getLastNonConfigurationInstance(); // Check if app still loading
+	    if (data != null) {
+	    	if (data.showSplashScreen) {  
+	    		if (getIntent().getIntExtra("splash_screen", 0) != 1) showSplashScreen();
+	        }  
+		setContentView(R.layout.activity_main_menu);  
+	    } else {  
+	    	if (getIntent().getIntExtra("splash_screen", 0) != 1) showSplashScreen();
+	    //Splash Code end
+			 
+		//other relevent stuff can go here
 		setContentView(R.layout.activity_main_menu);
+		}
 		
-Button match = (Button) findViewById(R.id.match_button);
-Button quick_match = (Button) findViewById(R.id.quick_match_button);
-Button training = (Button) findViewById(R.id.training_button);
-Button view_matches = (Button) findViewById(R.id.viewMatches_button);
-PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+		 
+		
+		Button match = (Button) findViewById(R.id.match_button);
+		
+		Button quick_match = (Button) findViewById(R.id.quick_match_button);
+		
+		Button training = (Button) findViewById(R.id.training_button);
+		
+		Button view_matches = (Button) findViewById(R.id.viewMatches_button);
+		
+		PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+		
 
 
 	match.setOnClickListener(new OnClickListener() {
@@ -100,6 +128,71 @@ startActivity(theIntent);
 		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
 	}
+	
+	
+	//Splash Code Start
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+	    MyStateSaver data = new MyStateSaver();
+	    
+	    if (mSplashDialog != null) {
+	    	
+	        data.showSplashScreen = true;
+	        removeSplashScreen();
+	    }
+	    return data;
+	}
+	 
+
+ // Removes dialog that displays the splash screen
+	 
+	protected void removeSplashScreen() {
+		 	    
+	    if (mSplashDialog != null) {
+			 	    
+	        mSplashDialog.dismiss();
+			 	    
+	        mSplashDialog = null;
+			 	    
+	    }
+	}
+	 
+	
+	//Shows the splash screen over the full Activity
+	 
+	protected void showSplashScreen() {
+		 
+		mSplashDialog = new Dialog(this, R.style.SplashScreen); // From res/values/styles.xml
+		 
+	    mSplashDialog.setContentView(R.layout.splashscreen);
+		 	    
+	    mSplashDialog.setCancelable(false);
+		 	    
+	    mSplashDialog.show();
+		 	    
+	     
+	    // Set Runnable to remove splash screen 
+	    final Handler handler = new Handler();
+		 	    
+	     handler.postDelayed(new Runnable() {
+	      
+	      public void run() {
+	 		 	    
+	        removeSplashScreen();
+			 
+	      }
+	    }, 3000); 
+		 	    
+	}
+
+	// class for storing important data across config changes
+	 
+	private class MyStateSaver {
+
+	    public boolean showSplashScreen = false;
+	}	
+
+
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
